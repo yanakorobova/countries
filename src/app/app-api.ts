9a2d3a846cabde77e226e05d3658eb4a0c5097b0
yanakorobova <08mickey01@gmail.com>
2023-03-21 21:33:59 +0300

    added api

---
+++ src/app/app-api.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const instance = axios.create({
+    baseURL: "https://restcountries.com/v2/"
+})
+export const appAPI = {
+    getCountries(){
+        return instance.get('all?fields=name,capital,flags,population,region')
+    },
+    getCountryByName(name:string){
+        return instance.get(`name/${name}`)
+    }
+}
