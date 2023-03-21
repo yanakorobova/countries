c49196b44a88adab01d98f827c58d3601c3bc13b
yanakorobova <08mickey01@gmail.com>
2023-03-21 21:36:35 +0300

    refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "pages/HomePage";
+import {Details} from "pages/Details";
+import {Main} from 'common/components/Main/Main';
+import {Header} from "common/components/Header/Header";
+import {NotFound} from "pages/NotFound";
+import {PATH} from "common/path/path";
+
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <Main>
+                <Routes>
+                    <Route path={PATH.HOME} element={<HomePage/>}/>
+                    <Route path={PATH.DETAILS} element={<Details/>}/>
+                    <Route path={PATH.ERROR} element={<NotFound/>}/>
+                </Routes>
+            </Main>
+        </>
+    );
 }
 
 export default App;
