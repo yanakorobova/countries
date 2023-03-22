fe72ab397c1f88a49d388e5ab4525969b667aab8
yanakorobova <08mickey01@gmail.com>
2023-03-22 21:14:53 +0300

    feat: added new persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,15 +4,46 @@ import thunk from "redux-thunk";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {countriesReducer} from "features/countries/countries-slice";
 import {controlsReducer} from "features/filterPanel/controls-slice";
+import {detailsReducer} from "features/details/detailsCountry-slice";
+import {themeReducer} from "features/theme/theme-slice";
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    persistReducer,
+    persistStore,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+
+
+const persistConfig = {
+    key: 'theme',
+    storage,
+    whitelist: ['theme']
+};
 
 const rootReducer = combineReducers({
     countries: countriesReducer,
-    controls: controlsReducer
+    controls: controlsReducer,
+    details: detailsReducer,
+    theme: themeReducer
 })
+const persistedReducer  = persistReducer(persistConfig, rootReducer);
+
+
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk)
+    reducer: persistedReducer ,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, PERSIST, PURGE, REHYDRATE, PAUSE, REGISTER],
+            ignoreActions: false,
+        },
+    }).prepend(thunk)
 })
+export const persistor = persistStore(store)
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
