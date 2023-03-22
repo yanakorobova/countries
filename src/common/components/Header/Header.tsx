dc56c5836a1e259682c79c7df81b4db781f635a1
yanakorobova <08mickey01@gmail.com>
2023-03-22 21:40:14 +0300

    feat: added reset filters

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -3,14 +3,20 @@ import s from './Header.module.scss'
 import {NavLink} from "react-router-dom";
 import {PATH} from "common/path/path";
 import {Theme} from "features/theme/Theme";
+import {useAppDispatch} from "app/store";
+import {resetFilters} from "features/filterPanel/controls-slice";
 
 
 export const Header = () => {
+    const dispatch = useAppDispatch()
+    const resetFiltersHandler = () => {
+        dispatch(resetFilters())
+    }
     return (
         <header className={s.header}>
             <div className={s.container}>
                 <div className={s.wrapper}>
-                    <NavLink to={PATH.HOME}>Where is the world?</NavLink>
+                    <NavLink to={PATH.HOME} onClick={resetFiltersHandler}>Where is the world?</NavLink>
                     <Theme/>
                 </div>
             </div>
